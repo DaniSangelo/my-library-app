6b9e474371446ef6d7990eecf3df812892c19dad
DaniSangelo <danielsangelo@hotmail.com>
2025-06-14 16:27:42 -0300

    feat: add application entrypoint

---
+++ src/interfaces/http/server.ts
@@ -0,0 +1,8 @@
+import bookRoutes from './routes/book-routes';
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+app.use('/api', bookRoutes);
+
+export default app;
