db2b1b98fe94663c7cc743668605aecc95392da6
DaniSangelo <danielsangelo@hotmail.com>
2025-06-14 16:33:29 -0300

    style: fix code style

--- src/interfaces/http/routes/book-routes.ts
+++ src/interfaces/http/routes/book-routes.ts
@@ -1,14 +1,14 @@
-import { Router } from "express"
-import { ListBooksByDeviceUseCase } from "@src/application/use-cases/list-books-by-device"
-import { PrismaBookRepository } from "@src/infrastructure/database/repositories/prisma-book.repository"
+import { Router } from 'express'
+import { ListBooksByDeviceUseCase } from '@src/application/use-cases/list-books-by-device'
+import { PrismaBookRepository } from '@src/infrastructure/database/repositories/prisma-book.repository'
 
 const router = Router()
 const bookRepository = new PrismaBookRepository()
 
-router.get('/books/:id', async(req, res) => {
-    const listBooksByDeviceUseCase = new ListBooksByDeviceUseCase(bookRepository);
-    const books = await listBooksByDeviceUseCase.execute({deviceId: 'e1'});
-    res.json(books);
+router.get('/books/:id', async (req, res) => {
+  const listBooksByDeviceUseCase = new ListBooksByDeviceUseCase(bookRepository)
+  const books = await listBooksByDeviceUseCase.execute({ deviceId: 'e1' })
+  res.json(books)
 })
 
-export default router;
+export default router
--- src/interfaces/http/server.ts
+++ src/interfaces/http/server.ts
@@ -1,8 +1,8 @@
-import bookRoutes from './routes/book-routes';
-import express from 'express';
+import bookRoutes from './routes/book-routes'
+import express from 'express'
 
-const app = express();
-app.use(express.json());
-app.use('/api', bookRoutes);
+const app = express()
+app.use(express.json())
+app.use('/api', bookRoutes)
 
-export default app;
+export default app
