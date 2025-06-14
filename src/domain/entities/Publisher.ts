fbb18c52720efeb289bb701ae8f150442f446446
Daniel Sângelo <danielsangelo@hotmail.com>
2025-06-14 09:05:05 -0300

    feat: add static method on Publisher entity

--- src/domain/entities/Publisher.ts
+++ src/domain/entities/Publisher.ts
@@ -1,14 +1,30 @@
+import { randomUUID } from "node:crypto";
+
 //todo: improve entity
 export interface PublisherProps {
-    id: string;
+    id?: string;
     name: string;
 }
   
 export class Publisher {
-    constructor(private props: PublisherProps) {}
+    private constructor(private props: PublisherProps) {
+        this.validate(props);
+        this.props = props;
+    }
+
+    static create(props: PublisherProps) {
+        return new Publisher(props);
+    }
+
+    private validate(props: PublisherProps) {
+        if (!props.name) {
+            throw new Error("Name is required");
+        }
+        props.id = props.id ?? randomUUID()
+    }
 
     get id() {
-        return this.props.id;
+        return this.props.id!;
     }
 
     get name() {
